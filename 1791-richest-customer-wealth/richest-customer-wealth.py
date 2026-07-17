9c2acd2c8bd052b90cdd13f6d847617881dfb7bc
Yash Patankar <yashrpatankar24@gmail.com>
2026-07-18 01:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (94.30%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        ans = 0
+
+        for account in accounts:
+
+            ans = max(ans, sum(account) )
+
+        return ans
